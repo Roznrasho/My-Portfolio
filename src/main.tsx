0f4e50f5023af14cb29f6f4aba6b1512a63cf880
Roznrasho <roznrasho@gmail.com>
2025-03-20 09:16:59 +0100

    refactor: update routing structure and remove unused components; add new page components for Home, About, Projects, and Contact

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import  Home  from './pages/Home'
+import About from './pages/About';
+import Contact from './pages/contact';
+import Projects from './pages/Projects';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
