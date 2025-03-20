08b21eed3721931c68fb2c49086dca409ee8ed13
Roznrasho <roznrasho@gmail.com>
2025-03-20 10:22:13 +0100

    fix: rename contact page file to match component casing and remove old contact file

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import './index.css'
 import  Home  from './pages/Home'
 import About from './pages/About';
-import Contact from './pages/contact';
+import Contact from './pages/Contact';
 import Projects from './pages/Projects';
 
 createRoot(document.getElementById('root')!).render(
